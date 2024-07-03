aa691560665cdeeff1fdbdabb0bcff29e08e69aa
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-07-03 16:31:57 +0300

    fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+import express from "express";
+import router from "./routes/index";
 
-const server = express();
+const app = express();
+const port = 5000;
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+app.use(express.json());
 
-export default server;
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
