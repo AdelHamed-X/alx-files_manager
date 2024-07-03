285160d5403d9d4c681a18e552bbf259cc16b6ff
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-07-03 16:10:25 +0300

    Adding app controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,18 @@
-/* eslint-disable import/no-named-as-default */
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
 
-export default class AppController {
-    static getStatus(req, res) {
-        res.status(200).json({
-            redis: redisClient.isAlive(),
-            db: dbClient.isAlive(),
-        });
-    }
+class AppController {
+  static getStatus(req, res) {
+    res
+      .status(200)
+      .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
 
-    static getStats(req, res) {
-        Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-            .then(([usersCount, filesCount]) => {
-                res.status(200).json({ users: usersCount, files: filesCount });
-            });
-    }
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return res.status(200).send({ users, files });
+  }
 }
+
+module.exports = AppController;
